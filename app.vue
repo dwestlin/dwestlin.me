078d4e15957120f8fdb93c430aca25d8e9cc9c19
Daniel Westlin <daniel.westlin@cybercom.com>
2022-02-20 15:49:01 +0100

    feature: adding the landing page.

--- app.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <NuxtWelcome />
-    Hello everyone. Welcome to my new website.
-  </div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>My personal website</h1>
+  <p>This is a test</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
